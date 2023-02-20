c88c84393969ff41ed7bc84ef63917fda29816cb
Fiifi Dawson <edem.fiifi.dawson@gmail.com>
2023-02-20 13:55:43 +0000

    Update

--- TechElevatePlan/Codes/Insertion_Sort.py
+++ TechElevatePlan/Codes/Insertion_Sort.py
@@ -1,15 +1,25 @@
 def insertion_sort(array):
     # Start an iteration starting from index 1 (second element)
     for index in range(1, len(array)):
-
+        # Store the current element in a temporary variable
         temp_value = array[index]
-        position = index - 1
+        # Store the index of the previous element
+        # This is the index of the element that we will compare with the temp_value
+        position = index - 1 #Decrementing index
+        # Loop through the array until we reach the beginning of the array
         while position >= 0:
+            # If the current element is greater than the temp_value
             if array[position] > temp_value:
+                # Shift the left element to the right
                 array[position + 1] = array[position]
+                # Decrement the position by 1 
+                #to compare the temp_value with the next element
                 position = position - 1
 
+            # If the current element is less than the temp_value
+            # We have found the correct position for the temp_value
             else:
                 break
+            # Insert the temp_value into the gap
             array[position + 1] = temp_value
         return array
--- TechElevatePlan/Reading_Notes/Chapter_6.md
+++ TechElevatePlan/Reading_Notes/Chapter_6.md
@@ -1,3 +1,29 @@
 ### Chapter 6 [20/Feb/2023]  
 
 ## Insertion Sort
+Insertion sort starts from the second element or index 1. It stores the value at index 1 in a temporary position. It compares it with the element at the left(index 0) to check wether it is greater or less then it. If the value at index 1 is less than the value to the left, it swaps else it remains there. For subsequent iterations, it moves to the next index and checks all the values to its left and compares to make the necessary swaps. This continues until the loop ends.
+
+Four types of steps occur in Insertion Sort:
+- removals (Worst case: Descending order)
+- comparisons (1....+.N - 1)(N^2 / 2)
+- shifts
+- insertions
+
+Worst Case Scenarios
+    N^2 / 2 - Comparisons
++   N^2 / 2 - Shifts
+----------------------
+    N^2 steps
+
+
+N^2 comparisons and shifts combined
+    N - 1 removals
++   N - 1 insertions
+_____________________________
+N^2 + 2N - 2 steps
+
+Simplify this as O(N^2+ N)
+
+                 Best Case|  Average Case|  Worst Case
+Selection Sort - N^2 / 2  |    N^2 / 2   |  N^2 / 2
+Insertion Sort -     N    |    N^2 / 2   |  N^2
